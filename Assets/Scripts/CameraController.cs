742490a94666a7eb068239f08ff68e08e65d4523
Max22006 <134130576+Max22006@users.noreply.github.com>
2026-05-15 10:06:56 +0200

    animation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,9 +2,9 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Vector3 startPosition;
+    public Vector3 startPosition; // ESTO NO HACE FALTA PARA EL Examen del 22
 
-    private Transform cameraTarget;
+    private Transform cameraTarget; // public , pero hay que arrastrar el objetivo en el menu de unity
     public Vector3 cameraOffset;
     
     public Vector2 minCameraPosition;
@@ -13,15 +13,15 @@ public class CameraController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        cameraTarget = GameObject.Find("Mario_0").GetComponent<Transform>();
-        //cameraTarget = GameObject.FindGameObjectWithTag("Player");
+        cameraTarget = GameObject.Find("Mario_0").GetComponent<Transform>(); // esto no hace falta si pongo en public lo anterior
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position = cameraTarget.position + cameraOffset;
-        //transform.position = new Vector3(cameraTarget.position.x, 0, 0) + cameraOffset;
+        //transform.position = cameraTarget.position + cameraOffset; //esto es la aversion facil, no se necesita los limites solo el cameraTarget y el cameraOffset.
+
        if (cameraTarget != null)
        {
            Vector3 desiredPosition = cameraTarget.position + cameraOffset;
@@ -31,7 +31,7 @@ public class CameraController : MonoBehaviour
 
         Vector3 clampedPosition = new Vector3(clampX, clampY, desiredPosition.z);
 
-        transform.position = clampedPosition; 
+        transform.position = clampedPosition;
        }
        
     }
